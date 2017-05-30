c2d36dbfec20f655d6f7305a6f3f9daa31bf6c98
Bernard Wolff <bernard.wolff@emarketer.com>
2017-05-30 16:20:19 -0400

    upgrade to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
 	entry: './index.js',
 	output: {
-		filename: 'bundle.js',
-		path: './build'
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build')
 	},
-	module: {
-		loaders: [{test: /\.js$/, exclude: /node_modules/, loader: 'babel'}]
-	}
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, exclude: /node_modules/, loader: 'css-loader'}
+    ]
+  }
 }
